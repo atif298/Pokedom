ac3ac71efc674bc345d9d8558d4d1d65042a3110
atif298 <atifuddin03@gmail.com>
2022-08-23 16:36:39 +0100

    notes removed

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 import {pokemonArray} from "./data/pokemon.js";
 
-console.log(pokemonArray[1]);
-
 const pokemonContainer = document.querySelector(".card-container");
 
-
-// pokemon.name = pokemon.name.charAt(0).toUpperCase()
-
-
 const displayCard = (pokemon) => {
-    
-    //charAt(0) will return the first character of each name and capitalise
-    //then add rest of the name apart from first character using slic(1)
     const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    
     pokemonContainer.innerHTML +=
     `
     <div class="card">
@@ -25,6 +15,4 @@ const displayCard = (pokemon) => {
     </div>
     `
 };
-
-
 pokemonArray.forEach(pokemon => displayCard(pokemon));
